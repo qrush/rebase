7af6e52c094e7b42304a9ccae6d4ec8396be3d77
Nick Quaranto <nick@quaran.to>
2008-10-25 16:08:38 -0400

    Parsing the feed for the last week!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,17 +16,45 @@ class EventsController < ApplicationController
   
   
   def create
-    feed = FeedNormalizer::FeedNormalizer.parse open('http://github.com/timeline.atom')
-    feed.clean!
     
-    feed.entries.each do |entry|
-      event = Event.new
-      event.kind = entry.id.scan(/[A-Za-z]+Event/).first.gsub("Event", "").downcase
-      event.published = entry.date_published
-      event.author = entry.author.split.first
-      event.title = entry.title
-      event.message = entry.content
-      event.save
+    page = 1
+    parse = true
+    
+    parse_feed = lambda do |p|   
+      begin
+        FeedNormalizer::FeedNormalizer.parse open("http://github.com/timeline.atom?page=#{p}")
+      rescue Exception => e
+        logger.info "Problem parsing the feed: #{e}"
+      end
+    end
+    
+    stop_date = Date.today.to_datetime
+    start_date = stop_date - 1.week
+    
+    logger.info ">>>>>>>> Start: #{start_date} Stop: #{stop_date}"
+    while parse
+      feed = parse_feed.call(page)
+      logger.info ">>>>>>>>> Parsing Page #{page}"
+      
+      if( parse = (feed && !feed.entries.empty?) )
+        feed.entries.each do |entry|
+          event = Event.new
+          event.published =entry.date_published.to_datetime
+          
+          if event.published >= start_date && event.published <= stop_date
+            event.kind = entry.id.scan(/[A-Za-z]+Event/).first.gsub("Event", "").downcase
+            event.author = entry.author.split.first
+            event.title = entry.title
+            event.message = entry.content
+            event.save
+          elsif event.published < start_date
+            parse = false
+            break
+          end
+        end
+      end
+      
+      page = page + 1
     end
     
     redirect_to events_path
@@ -47,9 +75,15 @@ class EventsController < ApplicationController
   end
   
   def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    flash[:notice] = "Successfully destroyed event."
+    if params[:id].to_i > 0
+      @event = Event.find(params[:id])
+      @event.destroy
+      flash[:notice] = "Successfully destroyed event."
+    else
+      Event.delete_all
+      flash[:notice] = "Successfully destroyed all events."
+    end
+    
     redirect_to events_url
   end
 end
