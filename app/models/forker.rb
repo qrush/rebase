d55b1bb83fd8c1de42ab19e86cd26dd9e9bf2313
Nick Quaranto <nick@quaran.to>
2008-11-23 09:48:18 -0500

    I think this is the first time I've used method_missing!

--- app/models/forker.rb
+++ app/models/forker.rb
@@ -1,5 +1,8 @@
 class Forker < ActiveRecord::Base
-  has_many :events
-
+  has_many :events do 
+    def method_missing(m, *args)
+      Event.count(:conditions => ['forker_id = ? and kind = ?', proxy_owner.id, m.to_s.singularize])
+    end
+  end
 
 end
